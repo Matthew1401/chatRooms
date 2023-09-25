2ff2b14b8592495b41f55ca80a1f297282025f83
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-09-25 10:56:27 +0200

    I am trying to do something great

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<!-- 
+  TODO: 1.Poczytać o tworzeniu czatów.
+  TODO: 2.Ogarnąć websockety.
+ -->
+
+<script setup>
+  
+</script>

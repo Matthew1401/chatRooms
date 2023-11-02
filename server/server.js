7eeccb74033568130ce6af899c44112f732ab17d
Matthew1401 <mateusz.konieczny1401@gmail.com>
2023-11-02 17:45:11 +0100

    loasd

--- server/server.js
+++ server/server.js
@@ -8,10 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      `https://mateuszkonieczny.netlify.app`,
-      "http://172.20.10.2:5173/",
-    ],
+    origin: "*",
   },
 });
 
